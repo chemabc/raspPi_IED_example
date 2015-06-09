75d8939185dc69e5773054fbdbad341f491e42e2
chema blanco <chema@etcinventions.com>
2015-06-09 14:49:02 +0000

    cambios en simpleLedGpio y simpleGpioButton

--- simpleGpioButton.py
+++ simpleGpioButton.py
@@ -12,21 +12,21 @@ import time
 import RPi.GPIO as GPIO #import GPIO library
 
 GPIO.setmode(GPIO.BOARD) #use board pin numering
-GPIO.setup(7, GPIO.OUT) #GPIO 7 to OUT
-GPIO.output(7, True) #Turn on GPIO 7
+GPIO.setup(7, GPIO.IN) #GPIO 7 to OUT
+
+currentInput = GPIO.input(7)
+lastInput = currentInput
+
+while (True):
+    if not GPIO.input(7):
+        print("button pressed")
+    '''
+    currentInput = GPIO.input(7)
+    if(currentInput is not lastInput)
+        print("Cambio to: " + str(currentInput))
+        lastInput = currentInput
+    '''
+GPIO.cleanup()
 
 
-def blinkLed():
-    print("Inciamos ejecucion...")
-    iteracion = 0
-    while iteracion < 5:
-        GPIO.output(7, False)
-        time.sleep(1) # 1 segundo
-        GPIO.output(7, True)
-        time.sleep(1) # 1 segundo
-        iteracion += 1
-    print("Ejecucion finalizada...")
-    GPIO.cleanup()
-
-blinkLed()
 
